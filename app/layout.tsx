44cb133c08f2048a93b72b8a7d4f165d374fc6bd
Marckenleydev <marckenlygbeau@gmail.com>
2026-02-19 02:16:49 -0800

    make whatsaap availlable everywhere in the page

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import { Providers } from "./providers";
+import Image from "next/image";
 
 
 
@@ -14,6 +15,7 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+    const message = "Hello I’m interested in your luxury car rental services. Can you share availability and pricing? ";
   return (
     <html lang="en">
       <body
@@ -22,6 +24,19 @@ export default function RootLayout({
         <Providers>
           {children}
         </Providers>
+        <div className="fixed bottom-6 right-6 z-9999 pointer-events-auto">
+        <a
+          aria-label="Chat on WhatsApp"
+          href={`https://wa.me/971559990003?text=${encodeURIComponent(message)}`}
+          target="_blank"
+          rel="noopener noreferrer"
+          className="w-16 h-16 rounded-full bg-white flex items-center justify-center shadow-2xl hover:scale-110
+          transition-transform
+          sm:animate-bounce"
+        >
+          <Image src="/whatsapp.svg" alt="WhatsApp" width={42} height={42} />
+        </a>
+      </div>
       </body>
     </html>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -11,7 +11,7 @@ import Footer from "./components/Footer";
 import Image from "next/image";
 
 export default function Home() {
-  const message = "Hello I’m interested in your luxury car rental services. Can you share availability and pricing? ";
+
   return (
     <main className="">
       <HeroSection />
@@ -24,19 +24,7 @@ export default function Home() {
       <FaqSection />
       <Footer />
 
-      <div className="fixed bottom-6 right-6 z-9999 pointer-events-auto">
-        <a
-          aria-label="Chat on WhatsApp"
-          href={`https://wa.me/971559990003?text=${encodeURIComponent(message)}`}
-          target="_blank"
-          rel="noopener noreferrer"
-          className="w-16 h-16 rounded-full bg-white flex items-center justify-center shadow-2xl hover:scale-110
-          transition-transform
-          sm:animate-bounce"
-        >
-          <Image src="/whatsapp.svg" alt="WhatsApp" width={42} height={42} />
-        </a>
-      </div>
+      
     </main>
   );
 }
